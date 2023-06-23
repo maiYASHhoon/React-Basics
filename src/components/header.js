1f32a9b7ba54e90b9190eacd3946f7b48645244f
maiYASHhoon <yash280801@gmail.com>
2023-06-23 17:43:00 +0530

    Dynamic Styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,8 @@
 import React, {Component} from "react";
 
-/*const inputChange = () => {
-    console.log("I Was Changed");
-};*/
-
 class Header extends Component {
     state = {
+        active: false,
         keywords: "",
     };
 
@@ -14,13 +11,20 @@ class Header extends Component {
             keywords: event.target.value,
         });
     };
+    changeColor = () => {
+        this.setState({
+            active: this.state.active ? false : true,
+        });
+    };
 
     render() {
         return (
             <>
-                <header>
+                {/* <header style={{background: `${this.state.active ? "red" : "blue"}`}}> */}
+                <header className={this.state.active ? "active" : "not-active"}>
                     <div className="logo">Code News</div>
                     <input onChange={(e) => this.inputChange(e)} />
+                    <button onClick={() => this.changeColor()}>Change It</button>
                 </header>
             </>
         );
