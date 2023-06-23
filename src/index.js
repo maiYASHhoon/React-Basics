897d46b88b8a2c902553ccf5e3a1db7733533812
maiYASHhoon <yash280801@gmail.com>
2023-06-23 16:38:52 +0530

    Props and Components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./styles/styles.css";
 
 import Header from "./components/header";
 import NewsList from "./components/news_list";
+import Footer from "./components/footer";
 import JSON from "./db.json";
 
 class App extends Component {
     state = {
         news: JSON,
+        footerText: "I am a happy footer",
     };
     render() {
         // console.log(this.state);
@@ -16,6 +18,7 @@ class App extends Component {
             <div className="hey">
                 <Header />
                 <NewsList news={this.state.news} />
+                <Footer footerText={this.state.footerText} />
             </div>
         );
     }
